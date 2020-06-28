cde52fe633571e78e1235b42bcb5a46e124d4fac
M0rqpEy <agaga3041@gmail.com>
2020-06-28 14:53:02 +0300

    added trailing slash

--- news/urls.py
+++ news/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     #Posts
-    path('posts/<int:post_id>',
+    path('posts/<int:post_id>/',
          views.PostDetailView.as_view(),
          name='post_detail'),
     path('posts/', views.PostListView.as_view(), name='posts_list'),
@@ -16,7 +16,7 @@ urlpatterns = [
     path('posts/<int:post_id>/comments/',
          views.CommentListView.as_view(),
          name='comments_list'),
-    path('posts/<int:post_id>/comments/<int:comment_id>',
+    path('posts/<int:post_id>/comments/<int:comment_id>/',
          views.CommentDetailView.as_view(),
          name='comment_detail'),
 ]
